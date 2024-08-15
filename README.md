fb4ddc1670ebb4215803028f86da2be80353e096
Ibrahim1022 <ibrahimhusseinjama1995@gmail.com>
2024-08-15 22:42:37 +0300

    Here is my answer.

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Fundamental_Javascript-Exercise" 
---
+++ script.js
@@ -0,0 +1,12 @@
+let num1 = 45;
+let num2 = 32;
+let num3 = 67;
+
+// Determine which number is the largest
+if (num1 > num2 && num1 > num3) {
+    console.log('The largest number out of the three numbers is ' + num1);
+} else if (num2 > num1 && num2 > num3) {
+    console.log('The largest number out of the three numbers is ' + num2);
+} else {
+    console.log('The largest number out of the three numbers is ' + num3);
+}
